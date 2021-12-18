0d034b3c46efd8c931013d282d5e0d1a5ff4a05e
Jens Lindqvist <jens@varvet.com>
2021-12-18 10:14:47 +0100

    1.4.5

--- clamp.rb
+++ clamp.rb
@@ -1,8 +1,10 @@
 class Clamp < Formula
   desc "command-line tool for local MAMP management"
   homepage "https://jide.github.io/clamp"
-  url "https://github.com/Plusboat/clamp.git", :using => :git, :tag => "1.4.4", :revision => "450ea3350ad019b6aaa243e290c4190d523692eb"
+  url "https://github.com/Plusboat/clamp.git", :using => :git, :tag => "1.4.5", :revision => "0308b31fc6fd921c083d4583f0c2c2dd12d3f162"
 
+  depends_on "httpd"
+  depends_on "php"
   depends_on "mariadb"
 
   def install
